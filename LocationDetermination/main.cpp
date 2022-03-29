fe84a9fd2b347f15bb47126a6fd834abaa97355b
rockbeen <62306907+rockbeen@users.noreply.github.com>
2022-03-29 21:17:33 +0300

    Add files via upload

---
+++ LocationDetermination/main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Location determination");
+    w.setFixedSize(830,650);
+
+
+
+
+    w.show();
+    return a.exec();
+}
